a3a88cf17d60a7011e06c2c3dfa6d63c244f0abf
Lucas Rezende <lucasrezende.cesar@gmail.com>
2023-07-21 12:10:43 -0300

    User statistic

--- time_to_answer/app/controllers/site/answer_controller.rb
+++ time_to_answer/app/controllers/site/answer_controller.rb
@@ -1,5 +1,7 @@
 class Site::AnswerController < SiteController
   def question
     @answer = Answer.find(params[:answer])
+    UserStatistic.set_statisctic(@answer, current_user)
+    end 
   end
-end
+
